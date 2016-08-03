985a18c6cd204830beea5f494b40649d5aad17be
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-08-03 11:29:31 -0500

    se actualiza el archivo con el que actualmente esta funcionando en linea, ya que habia olvidado actualizarlo

--- www/js/submit.php
+++ www/js/submit.php
@@ -1,26 +1,40 @@
 <?php
+include('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
+include('/usr/share/php/libphp-phpmailer/class.smtp.php');
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
+if(empty($_POST['name'])                ||
+   empty($_POST['email'])               ||
+   empty($_POST['message'])     ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
-	return false;
+        echo "No arguments Provided!";
+        return false;
    }
-	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 $company = $_POST['company'];
-
-// Create the email and send the message
-$to = 'conferexmx@gmail.com'; // Add your email address in between the '' replacing jennpereira13@gmail.com - This is where the form will send a message to.
-$cc = 'jesuscervin@icloud.com';
 $email_subject = "Conferex Forma de contacto:  $name";
-$email_body = "Has recibido un nuevo mensaje a traves de la forma de contacto del sitio de Conferex.mx.\n\n"."A continuacion los detalles:\n\nNombre: $name\n\nEmpresa:\n$company\n\nEmail: $email_address\n\nMensaje:\n$message";
-$headers = "From: mensajes@conferex.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$email_body = "<h2>Has recibido un nuevo mensaje a traves de la forma de contacto del sitio de Conferex.mx</h2> <h3>A continuacion los detalles: </h3> <ul><li>Nombre: $name </li><li>Empresa: $company</li><li>Email: $email_address </li><li>Mensaje: $message </li></ul>";
+// creating email & sending
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->IsHTML(true);
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "conferexmx@gmail.com";
+$mail->Password = "gddhuquexafoenhm";
+$mail->SetFrom($email_address);
+$mail->Subject = $email_subject;
+$mail->Body = $email_body;
+$mail->AddAddress("aenciso@conferex.mx");
+ if(!$mail->Send()) {
+    return true;
+ } else {
+    return false;
+ }
 ?>
